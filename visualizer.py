59edd5657cd6d99f42df57cf8418c9a033a8fd91
Sudip Vikram Adhikari <32861861+SudipVikram@users.noreply.github.com>
2026-09-12 20:42:44 +0545

    grid and encoder data display integrated

--- visualizer.py
+++ visualizer.py
@@ -17,7 +17,48 @@ odometry_data = serialData.ucontroller(serialData,port='COM7',baudrate=115200,ti
 canvas.window_title("Punte Robot - Visualizer")
 
 while True:
+    # canvas background
     canvas.background_color("white")
+
+    #========
+    # GRID
+    #========
+    # columns
+    for i in range(0, canvas.wwidth, 150):
+        canvas.draw_line(start=(i,0),end=(i,canvas.wheight),color="lightgray",width=1)
+
+    # rows
+    for j in range(0, canvas.wheight, 150):
+        canvas.draw_line(start=(0,j),end=(canvas.wwidth,j),color="lightgray",width=1)
+
+    #=========
+    # ODOMETRY DATA
+    #=========
+    # placeholder for odometry data
+    canvas.draw_rect(color="cyan",org=(1150,10),width=150,height=75,border_thickness=0,border_radius=10)
+
+    data_from_serial = odometry_data.receive_serial_data()
+
+    if data_from_serial is not None:
+        try:
+            line = data_from_serial.strip() # remove carriage return \n
+
+            if line.startswith("L:") and ", R:" in line:
+                # splitting the data step by step
+                l_encoder = line.split(", R:")[0].strip()      # L:12
+                r_encoder = line.split(", R:")[1].strip()      # 34
+
+                # parse each value
+                left = int(l_encoder.replace("L:",""))
+                right = int(r_encoder)
+
+        except Exception:
+            print("Bad data packet: ",data_from_serial)
+
+    # encoder data
+    canvas.draw_text(text="Encoder Data",font_size=16,color=(0,0,0),xpos=1155,ypos=15)
+    canvas.draw_text(text=f"Left: {left}",font_size=16,color=(84,84,84),xpos=1155,ypos=35)
+    canvas.draw_text(text=f"Right: {right}",font_size=16,color=(84,84,84),xpos=1155,ypos=55)
 
     # fps
     canvas.set_fps(60)
